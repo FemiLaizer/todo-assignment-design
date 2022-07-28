9ff1d388277a9cd6c7be8918141c5393dd332d75
Femi Laizer <femilaizer@gmail.com>
2022-07-28 02:17:03 +0100

    https://github.com/FemiLaizer/todo-assignment-design.git

---
+++ main.js
@@ -0,0 +1,27 @@
+let searchBox = document.querySelector(".search-box");
+let addTODO = document.querySelector(".addTODO");
+
+addTODO.addEventListener("click", function () {
+    createToDo();
+});
+
+
+searchBox.addEventListener('keypress', (e) => {
+    if (e.which === 13) {
+        createToDo();
+    }
+})
+
+function createToDo() {
+    if (searchBox.value !== "") {
+        let ul = document.createElement("ul");
+        let li = document.createElement('li');
+        let span = document.createElement('span');
+        let p = document.createElement('p');
+
+        document.querySelector(".right").appendChild(ul);
+        ul.appendChild(li);
+        li.textContent = searchBox.value;
+        searchBox.value = "";
+    }
+}
